d4d1213fa729f47d369ac186f15ceafe5ef83a23
yeonjoonkim <yeonjoonkim@users.noreply.github.com>
2021-07-08 12:49:15 +1000

    All-in-One WP Migration Install
    All-in-One WP Migration Install has been implemented and test was sucessful

--- plugins/mp-timetable/admin/import/header.php
+++
@@ -1,2 +0,0 @@
-<div class="wrap">
-	<h1 class="wp-heading-inline"><?php _e('Import / Export Timetable Plugin Data', 'mp-timetable') ?></h1>
--- plugins/mp-timetable/admin/import/import.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if ( current_user_can('import') ) { ?>
-	<h3><?php _e('Import', 'mp-timetable') ?></h3><?php
-	wp_import_upload_form('admin.php?import=mptt-importer&amp;step=1');
-}
